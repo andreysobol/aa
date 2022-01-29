9b0dfa82552b555333256ee9ea4926b6ee21c94f
Andrey Sobol <andreysobol@protonmail.com>
2022-01-29 14:33:28 +0200

    Magma

--- aa.py
+++ aa.py
@@ -24,3 +24,25 @@ def difference(set1, set2):
 
 def subtract(subtracted_set, subtractor_set):
     return Set([i for i in subtracted_set if not is_element(i, subtractor_set)])
+
+def cartesian_product(set1, set2):
+    return [[Set([i, j]) for j in set2] for i in set1]
+
+
+# Magma (groupoid) https://en.wikipedia.org/wiki/Magma_(algebra)
+
+def is_closed(target_set, operation, arguments):
+    if arguments == 1:
+        return all([operation(a) in target_set for a in target_set])
+    if arguments == 2:
+        return all(
+            [
+                (operation(param[0], param[1]) in target_set)
+                for param in cartesian_product(target_set, target_set)
+            ]
+        )
+    return None
+
+def is_magma(target_set, operation):
+    return is_closed(target_set, operation, 1)
+
