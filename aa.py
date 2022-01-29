8aa9f66b7f4dea53ba06db783d40a58bb3c261e9
Andrey Sobol <andreysobol@protonmail.com>
2022-01-29 15:03:28 +0200

    Group

--- aa.py
+++ aa.py
@@ -29,7 +29,7 @@ def subtract(subtracted_set, subtractor_set):
     return Set([i for i in subtracted_set if not is_element(i, subtractor_set)])
 
 def cartesian_product(set1, set2):
-    return [[Set([i, j]) for j in set2] for i in set1]
+    return Set([[Set([i, j]) for j in set2] for i in set1])
 
 
 # Magma (groupoid) https://en.wikipedia.org/wiki/Magma_(algebra)
@@ -48,3 +48,37 @@ def is_closed(target_set, operation, arguments):
 
 def is_magma(target_set, operation):
     return is_closed(target_set, operation, 1)
+
+
+# Group https://en.wikipedia.org/wiki/Group_(mathematics)
+
+def is_associative(target_set, operation):
+    bc = cartesian_product(target_set, target_set)
+    a_bc = cartesian_product(target_set, bc)
+    abc = [{
+        "a": item[0],
+        "b": item[1][0],
+        "c": item[1][1],
+    } for item in a_bc]
+    return all([
+        operation(operation(i["a"], i["b"]), i["c"]) == operation(i["a"], operation(i["b"], i["c"])) 
+        for i in abc
+    ])
+
+def is_identity_element(target_set, operation, identity_element):
+    ise = is_element(identity_element, target_set)
+    isi = all([operation(identity_element, j) == identity_element for j in target_set])
+    return ise and isi
+
+def is_inverse_operation(target_set, operation, identity_element, inverse_operation):
+    return all([
+        (operation(j, inverse_operation(j)) == operation(inverse_operation(j), j) == identity_element)
+        for j in target_set
+    ])
+
+def is_group(target_set, operation, identity_element, inverse_operation):
+    return all([
+        is_associative(target_set, operation),
+        is_identity_element(target_set, operation, identity_element),
+        is_inverse_operation(target_set, operation, identity_element, inverse_operation),
+    ])
